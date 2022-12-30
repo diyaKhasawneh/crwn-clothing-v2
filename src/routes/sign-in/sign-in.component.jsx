02781818e4d9402643e070bce3ab4d15ed757b59
unknown <diyakhasawneh@yahoo.com>
2022-12-30 13:24:18 -0800

    add fire base import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,19 +1,19 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
-const SignIn = () =>{
-const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    console.log(response);
-}
+import {
+  signInWithGooglePopup,
+  creatUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    creatUserDocumentFromAuth(user);
+  };
 
-
-
-    return(
-
-        <div>
-        <h1>sign in page</h1>
-        <button onClick={logGoogleUser}>sign in with google popup</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>sign in page</h1>
+      <button onClick={logGoogleUser}>sign in with google popup</button>
+    </div>
+  );
+};
 
 export default SignIn;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB5vYQRpdWwswtJon7LadYNr9BRdlWmOLA",
@@ -18,11 +19,19 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
-    prompt: 'select_account'
-
+  prompt: "select_account",
 });
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+export const creatUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+
+const userSnapshot = await getDoc(userDocRef);
+console.log(userSnapshot);
+console.log(userSnapshot.exists());
+};
